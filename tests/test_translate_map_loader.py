96474569bbf5c9a21eb410340099b786245892a5
ghjulia01 <colleoni_julie@hotmail.com>
2025-08-10 01:15:23 +0200

    pytest

---
+++ tests/test_translate_map_loader.py
@@ -0,0 +1,10 @@
+
+import json
+from features.text_pipeline import _load_translate_map
+
+def test_load_translate_map_file(tmp_path):
+    p = tmp_path / "map.json"
+    with open(p, "w", encoding="utf-8") as f:
+        json.dump({"black": "noir"}, f)
+    mp = _load_translate_map(str(p))
+    assert mp.get("black") == "noir"
