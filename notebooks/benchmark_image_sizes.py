6953cbd1501b4e9846d893c5e78d94fd1b6be782
ghjulia01 <colleoni_julie@hotmail.com>
2025-08-19 20:53:09 +0200

    from pathlib import Path

--- notebooks/benchmark_image_sizes.py
+++ notebooks/benchmark_image_sizes.py
@@ -10,6 +10,7 @@ import os, sys, time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
